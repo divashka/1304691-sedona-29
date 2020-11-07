665096074edcc6dd35ecc6b0fb44f35199419a66
divashka <66060704+divashka@users.noreply.github.com>
2020-11-07 14:32:58 +0300

    Дополнительные критерии

--- js/form-search.js
+++ js/form-search.js
@@ -4,6 +4,7 @@ var input_in = document.querySelector(".form__input_in");
 var input_out = document.querySelector(".form__input_out");
 var input_child = document.querySelector(".form__input_children");
 var input_adults = document.querySelector(".form__input_adults");
+var container = document.querySelector(".conclusion__form-container");
 
 let isStorageSupport = true;
 let storage = "";
@@ -16,7 +17,8 @@ try {
 
 button.addEventListener("click", function (evt) {
     evt.preventDefault();
-    form.classList.toggle("conclusion__form_visible");
+    form.classList.toggle("conclusion__form_hidden");
+    container.classList.toggle("pointer-none");
 });
 
 form.addEventListener("submit", function (evt) {
@@ -27,8 +29,8 @@ form.addEventListener("submit", function (evt) {
         form.classList.add("conclusion__error");
     } else {
         if (isStorageSupport) {
-            localStorage.setItem('Children', input_child.value);
-            localStorage.setItem('Adults', input_adults.value);
+            localStorage.setItem("Children", input_child.value);
+            localStorage.setItem("Adults", input_adults.value);
         }
     }
 });
